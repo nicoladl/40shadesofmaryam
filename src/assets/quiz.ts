a794dc3bf43185857d3a6b8802193e38424ecda5
Nicola De Lazzari <nicola.delazzari@arcual.art>
2023-11-20 22:28:47 +0100

    refactor: update data

--- src/assets/quiz.ts
+++ src/assets/quiz.ts
@@ -7,208 +7,398 @@ export const quiz: Array<Question> = [
             "22nd of September 1983",
             "27th of September 1983",
             "27th of November 1983",
-            "22nd of November 1983",
+            "22nd of November 1983"
         ],
-        correctAnswers: [0],
+        correctAnswers: [0]
     },
     {
         question: "Where was Maryam born?",
-        options: ["Tehran", "Isfahan", "Berlin", "Shiraz"],
-        correctAnswers: [0],
+        options: [
+            "Tehran",
+            "Isfahan",
+            "Berlin",
+            "Shiraz"
+        ],
+        correctAnswers: [0]
     },
     {
         question: "How many siblings does Maryam have?",
-        options: ["One", "Two", "Three", "None"],
-        correctAnswers: [1],
+        options: [
+            "One",
+            "Two",
+            "Three",
+            "None"
+        ],
+        correctAnswers: [3]
     },
     {
         question: "Which school or university did Maryam attend?",
-        options: ["ETH", "ETF", "SUPSI", "Private University of Tehran"],
-        correctAnswers: [0, 2, 3],
+        options: [
+            "ETH",
+            "ETF",
+            "SUPSI",
+            "Private University of Tehran"
+        ],
+        correctAnswers: [3]
+    },
+    {
+        question: "What is Maryam's second name?",
+        options: [
+            "Kosar",
+            "Gorbe",
+            "Pashtool",
+            "Ira"
+        ],
+        correctAnswers: [0]
     },
     {
         question: "Name countries she has lived",
-        options: ["Italy", "Switzerland", "Sweden", "France"],
-        correctAnswers: [1],
+        options: [
+            "Italy",
+            "Switzerland",
+            "Sweden",
+            "France"
+        ],
+        correctAnswers: [0, 1]
     },
     {
         question: "What musical instrument did Maryam play when she was young?",
-        options: ["Piano", "Guitar", "Violin", "Flute"],
-        correctAnswers: [0],
+        options: [
+            "Piano",
+            "Guitar",
+            "Violin",
+            "Flute"
+        ],
+        correctAnswers: [2]
     },
     {
         question: "Name a famous landmark she's visited",
-        options: ["South Africa", "Ecuador", "Sri Lanka", "Patagonia"],
-        correctAnswers: [1],
+        options: [
+            "South Africa",
+            "Ecuador",
+            "Sri Lanka",
+            "Patagonia"
+        ],
+        correctAnswers: [3]
     },
     {
         question: "Which country is at the top of Maryam's travel bucket list?",
-        options: ["Antarctica", "USA", "Japan", "Socotra"],
-        correctAnswers: [3],
+        options: [
+            "Antarctica",
+            "USA",
+            "Japan",
+            "Socotra"
+        ],
+        correctAnswers: [0]
     },
     {
         question: "What's the most adventurous thing she's ever done?",
-        options: ["Paragliding", "Ferrata F K6", "Taking the train without a ticket", "Skydiving from 15,000 feet"],
-        correctAnswers: [1],
+        options: [
+            "Paragliding",
+            "Ferrata F K6",
+            "Taking the train without ticket",
+            "Skydiving from 15,000 feet"
+        ],
+        correctAnswers: [3]
     },
     {
         question: "What's Maryam's signature dish or favorite meal?",
-        options: ["Iranian Lasagna", "Ghormeh sabzi", "Fesenjān", "Salad with everything"],
-        correctAnswers: [1],
+        options: [
+            "Iranian Lasagna (Loobia polo)",
+            "Ghormeh sabzi",
+            "Fesenjān",
+            "Salad with everything"
+        ],
+        correctAnswers: [2]
     },
     {
         question: "What's her favorite dessert?",
-        options: ["She doesn't like dessert", "Tiramisu", "Cheesecake", "Carrot cake"],
-        correctAnswers: [0],
+        options: [
+            "She doesn't like dessert",
+            "Tiramisu",
+            "Cheesecake",
+            "Carrot cake"
+        ],
+        correctAnswers: [1]
     },
     {
         question: "Favourite Maryam’s pizzeria?",
-        options: ["Dominos", "Napule", "San Gennaro", "Luigia"],
-        correctAnswers: [1],
+        options: [
+            "Dominos",
+            "Napule",
+            "San Gennaro",
+            "Luigia"
+        ],
+        correctAnswers: [2]
+    },
+    {
+        question: "What's Maryam's favorite bar in Zurich?",
+        options: [
+            "Bohemia",
+            "Bar am Wasser",
+            "Tibits",
+            "None of the above"
+        ],
+        correctAnswers: [0]
     },
     {
         question: "What is Maryam’s favorite cocktail?",
-        options: ["Margarita", "Old fashioned", "Caipirinha", "Negroni"],
-        correctAnswers: [0],
+        options: [
+            "Margarita",
+            "Old fashioned",
+            "Caipirinha",
+            "Negroni"
+        ],
+        correctAnswers: [3]
     },
     {
-        question: "What's the title of Maryam's favorite tv show?",
-        options: ["Friends", "Gilmore Girls", "Kardashian’s", "Big Bang Theory"],
-        correctAnswers: [0],
+        question: "What's the title of Maryam's favorite TV show?",
+        options: [
+            "Friends",
+            "This is us",
+            "Succession",
+            "Big Bang Theory"
+        ],
+        correctAnswers: [2]
     },
     {
-        question: "Which movie genre does Maryam prefer?",
-        options: ["Italian love stories", "Comedies", "Thrillers", "Musical"],
-        correctAnswers: [3],
+        question: "Which movie genres does Maryam prefer?",
+        options: [
+            "Italian love stories",
+            "Comedies",
+            "Thrillers",
+            "Musical"
+        ],
+        correctAnswers: [1]
     },
     {
         question: "What's Maryam's preferred way to spend a Saturday afternoon?",
-        options: ["Netflix", "Dancefloor", "Bohemia", "Reading"],
-        correctAnswers: [3],
+        options: [
+            "Netflix",
+            "Dancefloor",
+            "Bohemia",
+            "Reading"
+        ],
+        correctAnswers: [3]
     },
     {
         question: "Which famous person does Maryam admire the most?",
-        options: ["Elon Musk", "Bill Gates", "Michelle Obama"],
-        correctAnswers: [1],
+        options: [
+            "Elon Musk",
+            "Bill Gates",
+            "Michelle Obama",
+            "Maldini"
+        ],
+        correctAnswers: [2]
     },
     {
         question: "What's her favorite flower?",
-        options: ["Lilium", "Orchids", "Roses", "Sunflowers"],
-        correctAnswers: [3],
+        options: [
+            "Lilium",
+            "Orchids",
+            "Roses",
+            "Sunflowers"
+        ],
+        correctAnswers: [1]
     },
     {
         question: "What's the most recent movie she watched and loved?",
-        options: ["Barbie", "Oppenheimer"],
-        correctAnswers: [1],
+        options: [
+            "Barbie",
+            "Oppenheimer",
+            "Killers of the Flower Moon",
+            "The Marvels"
+        ],
+        correctAnswers: [2]
     },
     {
         question: "What's Maryam's favorite season?",
-        options: ["Fall (with all the cozy sweaters)", "Summer (for the beachwear)", "Spring (to break out the floral dresses)", "Winter (she loves layering)"],
-        correctAnswers: [0],
+        options: [
+            "Fall (with all the cozy sweaters)",
+            "Summer (for the beachwear)",
+            "Spring (to break out the floral dresses)",
+            "Winter (she loves layering)"
+        ],
+        correctAnswers: [0]
     },
     {
         question: "What's her most-used app on her phone?",
-        options: ["TikTok", "Instagram", "Skyscanner", "Sleepwatch"],
-        correctAnswers: [1],
+        options: [
+            "TikTok",
+            "Instagram",
+            "Skyscanner",
+            "Sleepwatch"
+        ],
+        correctAnswers: [1]
     },
     {
         question: "Which sport or physical activity does Maryam enjoy?",
-        options: ["Weight lift", "Running", "Swimming", "Bowling"],
-        correctAnswers: [3],
+        options: [
+            "Weight lift",
+            "Running",
+            "Swimming",
+            "Bowling"
+        ],
+        correctAnswers: [2]
     },
     {
         question: "What's Maryam's preferred mode of transportation?",
-        options: ["E-bike", "Bike", "Train", "Scooter"],
-        correctAnswers: [1],
+        options: [
+            "E-bike",
+            "Bike",
+            "Train",
+            "Scooter"
+        ],
+        correctAnswers: [0]
     },
     {
         question: "What's her favorite music composer?",
-        options: ["Ennio Morricone", "Hans Zimmer", "Alan Silvestri", "John Williams"],
-        correctAnswers: [1],
+        options: [
+            "Ennio Morricone",
+            "Hans Zimmer",
+            "Alan Silvestri",
+            "John Williams"
+        ],
+        correctAnswers: [1]
     },
     {
         question: "What's her favorite classic music composer?",
-        options: ["Mozart", "Beethoven", "Bach", "Vivaldi"],
-        correctAnswers: [0],
+        options: [
+            "Chopin",
+            "Beethoven",
+            "Bach",
+            "Vivaldi"
+        ],
+        correctAnswers: [0]
     },
     {
         question: "What's the title of her current job or profession?",
-        options: ["Business case owner", "Mechanical engineer", "Chief Executive Officer", "Vice President of Napping Strategies"],
-        correctAnswers: [1],
+        options: [
+            "Business case owner",
+            "Mechanical engineer",
+            "Chief Executive Officer",
+            "Vice President of Napping Strategies"
+        ],
+        correctAnswers: [1]
     },
     {
         question: "Which word best describes Maryam's approach to life?",
-        options: ["Adventurous", "Methodical", "Spontaneous", "Thoughtful"],
-        correctAnswers: [0],
+        options: [
+            "Adventurous",
+            "Methodical",
+            "Spontaneous",
+            "Thoughtful"
+        ],
+        correctAnswers: [0]
     },
     {
         question: "What would be Maryam's dream job if she weren't in her current profession?",
-        options: ["Travel blogger", "Chef", "Astronaut", "Marine biologist"],
-        correctAnswers: [0],
+        options: [
+            "Travel blogger",
+            "Chef",
+            "Astronaut",
+            "Marine biologist"
+        ],
+        correctAnswers: [0]
     },
     {
         question: "What's Maryam's favorite winter activity?",
-        options: ["Skiing or snowboarding", "Building snowmen", "Sipping hot cocoa by the fireplace", "Building igloos"],
-        correctAnswers: [0],
+        options: [
+            "Skiing or snowboarding",
+            "Building snowmen",
+            "Sipping hot cocoa by the fireplace",
+            "Building igloos"
+        ],
+        correctAnswers: [2]
     },
     {
-        question: "What's Maryam's favorite winter activity?",
-        options: ["Skiing or snowboarding", "Building snowmen", "Sipping hot cocoa by the fireplace", "Building igloos"],
-        correctAnswers: [0],
+        question: "Name the first job Maryam ever had",
+        options: [
+            "Bartender",
+            "IT administrator",
+            "Professor assistant",
+            "Bus driver"
+        ],
+        correctAnswers: [1]
     },
     {
-        question: "What's Maryam's favorite winter activity?",
-        options: ["Skiing or snowboarding", "Building snowmen", "Sipping hot cocoa by the fireplace", "Building igloos"],
-        correctAnswers: [0],
+        question: "Where would Maryam want to live?",
+        options: [
+            "Zurich",
+            "Switzerland",
+            "Swiss Alps",
+            "Helvetica"
+        ],
+        correctAnswers: [0]
     },
     {
-        question: "Which word best describes Maryam's approach to life?",
-        options: ["Adventurous", "Methodical", "Spontaneous", "Thoughtful"],
-        correctAnswers: [0],
+        question: "Who's Maryam's favorite comedian?",
+        options: [
+            "Dave Chappelle",
+            "Trevor Noah",
+            "Sebastian Maniscalco",
+            "Jerry Seinfeld"
+        ],
+        correctAnswers: [1]
     },
     {
-        question: "What would be Maryam's dream job if she weren't in her current profession?",
-        options: ["Travel blogger", "Chef", "Astronaut", "Marine biologist"],
-        correctAnswers: [0],
+        question: "What is one object Maryam can't live without?",
+        options: [
+            "Her wallet",
+            "Her headphone",
+            "Her PHONE",
+            "Her sunglasses"
+        ],
+        correctAnswers: [2]
     },
     {
-        question: "What's Maryam's favorite winter activity?",
-        options: ["Skiing or snowboarding", "Building snowmen", "Sipping hot cocoa by the fireplace", "Building igloos"],
-        correctAnswers: [0],
+        question: "Who is Maryam's favorite person?",
+        options: [
+            "Nicola",
+            "Her boyfriend",
+            "Partner at home",
+            "Mr. De Lazzari"
+        ],
+        correctAnswers: [3]
     },
     {
-        question: "What's Maryam's favorite winter activity?",
-        options: ["Skiing or snowboarding", "Building snowmen", "Sipping hot cocoa by the fireplace", "Building igloos"],
-        correctAnswers: [0],
+        question: "What is one thing Maryam always wanted to do but haven't done yet?",
+        options: [
+            "Bunji jumping",
+            "Adopt a pet",
+            "Learn a new language",
+            "Add here"
+        ],
+        correctAnswers: []
     },
     {
-        question: "What's Maryam's favorite winter activity?",
-        options: ["Skiing or snowboarding", "Building snowmen", "Sipping hot cocoa by the fireplace", "Building igloos"],
-        correctAnswers: [0],
+        question: "What's Maryam's favorite way to unwind after a long day?",
+        options: [
+            "Reading a book",
+            "Watching TV",
+            "Instagram",
+            "Cooking"
+        ],
+        correctAnswers: [0]
     },
     {
-        question: "Which word best describes Maryam's approach to life?",
-        options: ["Adventurous", "Methodical", "Spontaneous", "Thoughtful"],
-        correctAnswers: [0],
+        question: "What is Maryam's favorite food?",
+        options: [
+            "Souvlaki",
+            "Pizza",
+            "Spare ribs",
+            "Everything Asefeh cooks"
+        ],
+        correctAnswers: [1, 3]
     },
     {
-        question: "What would be Maryam's dream job if she weren't in her current profession?",
-        options: ["Travel blogger", "Chef", "Astronaut", "Marine biologist"],
-        correctAnswers: [0],
-    },
-    {
-        question: "What's Maryam's favorite winter activity?",
-        options: ["Skiing or snowboarding", "Building snowmen", "Sipping hot cocoa by the fireplace", "Building igloos"],
-        correctAnswers: [0],
-    },
-    {
-        question: "What's Maryam's favorite winter activity?",
-        options: ["Skiing or snowboarding", "Building snowmen", "Sipping hot cocoa by the fireplace", "Building igloos"],
-        correctAnswers: [0, 1, 3],
-    },
-    {
-        question: "What's Maryam's favorite bread?",
-        options: ["Dinkel", "Dunkel", "White Bread", "Aelplerbrot"],
-        correctAnswers: [1],
+        question: "What is the last thing that impressed Maryam?",
+        options: [
+            "Souvlaki",
+            "Pizza",
+            "Spare ribs",
+            "Everything Asefeh cooks"
+        ],
+        correctAnswers: [1]
     }
 ];
