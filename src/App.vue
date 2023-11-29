0e56a1a387954df6c245fcf785b892ff414ca255
Nicola De Lazzari <nicola.delazzari@arcual.art>
2023-11-29 22:24:38 +0100

    fix: test if the progressbar block the scroll

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import ProgressBar from "@/components/ProgressBar.vue";
 </script>
 
 <template>
-  <ProgressBar/>
   <RouterView />
 </template>
 
